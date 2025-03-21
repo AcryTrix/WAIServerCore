e20bc87849bd9c7cb0b300b18163db78ccc88b2e
acry <acrytrix@gmail.com>
2025-03-21 15:32:57 +0300

    www

--- src/main/java/org/wai/WAIServerCore.java
+++ src/main/java/org/wai/WAIServerCore.java
@@ -5,31 +5,19 @@ import org.wai.database.DatabaseManager;
 import org.wai.modules.AltsModule;
 import org.wai.modules.AutoRestartModule;
 import org.wai.modules.LinkingModule;
-import org.wai.modules.ProfileModule;
-import org.wai.modules.TradeModule;
-import org.wai.modules.TradeCommandHandler;
 
 public class WAIServerCore extends JavaPlugin {
     private DatabaseManager dbManager;
     private LinkingModule linkingModule;
     private AltsModule altsModule;
-    private ProfileModule profileModule;
     private AutoRestartModule autoRestartModule;
-    private TradeModule tradeModule;
 
     @Override
     public void onEnable() {
         dbManager = new DatabaseManager(getLogger());
         linkingModule = new LinkingModule(this, dbManager.getLinksConnection());
         altsModule = new AltsModule(this, dbManager.getAltsConnection());
-        profileModule = new ProfileModule(this);
         autoRestartModule = new AutoRestartModule(this);
-        tradeModule = new TradeModule(this); // Инициализация модуля обмена титулами
-
-        // Регистрация команд для обмена титулами
-        TradeCommandHandler tradeCommandHandler = new TradeCommandHandler(tradeModule);
-        getCommand("tradeaccept").setExecutor(tradeCommandHandler);
-        getCommand("tradedecline").setExecutor(tradeCommandHandler);
 
         altsModule.registerCommandsAndEvents();
         autoRestartModule.start();
